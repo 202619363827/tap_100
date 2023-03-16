e74f409adc99ea1018235c50e271860a18dd0b4f
bharti zade <bhartigzade@gmail.com>
2023-03-16 20:28:36 +0530

    tap on button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+// 
+import React, { useState } from "react";
+
+function CharacterLimit() {
+  const [text, setText] = useState("");
+  
+  function handleChange(event) {
+    const newText = event.target.value;
+    if (newText.length <= 100) {
+      setText(newText);
+      if (newText.length === 100) {
+        alert(" CONGRATULATIONs!!!!!!You have reached the character limit of 100!");
+      }
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <textarea value={text} onChange={handleChange} />
+      <p>{text.length} / 100 characters</p>
     </div>
   );
 }
 
-export default App;
+export default CharacterLimit;
+
